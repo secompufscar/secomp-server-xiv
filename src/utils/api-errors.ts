60afc8ce21f3828291eda1749041adabd5a05871
Enzo Murayama <enzomurayama@gmail.com>
2025-09-26 14:33:00 -0300

    refactor: block checkin for waitlist users

--- src/utils/api-errors.ts
+++ src/utils/api-errors.ts
@@ -10,6 +10,7 @@ export class ApiError extends Error {
 export enum ErrorsCode {
   BAD_REQUEST = 400,
   UNAUTHORIZED = 401,
+  FORBIDDEN = 403,
   NOT_FOUND = 404,
   CONFLICT = 409,
   INTERNAL_ERROR = 500,
